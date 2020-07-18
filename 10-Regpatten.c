c6fe0b40b0d4405d2111b887a846dcdb0e0619e4
ZJR19930222 <fibonacci_key7@sina.com>
2020-07-19 01:50:41 +0800

    一个比较完整的正则匹配

--- 10-Regpatten.c
+++ 10-Regpatten.c
@@ -3,134 +3,125 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define NUM_MAX_MATCH 20
 #define BLOCK 10
-#define nt '\0'
-#define star '*'
-#define dot '.'
-#define slashes '\\'
+#define NL '\0'
+#define STAR '*'
+#define DOT '.'
+#define SLASHES '\\'
 
-char* read_line(void);
-bool RegPatten(char *s, char *p);
-char* CheckSlashes(char *p, int len);
+struct MatchTerm {
+  int len;
+  char *pstring[NUM_MAX_MATCH];
+};
+char* Read_Line(void);
+bool IsRegMatch(char *s, char *p);
+char* DelErrorSlashes(char *p, int len);
+struct MatchTerm ComplMatch(char *s, char *p);
 
 int main(){
   char *String, *Patten;
-  printf("Enter target String>>");
-  String = read_line();
-  printf("Enter Patten String>>");
-  Patten = read_line();
-//printf("%s\n",String);
-//printf("%s\n",Patten);
-  char *NewPatten = CheckSlashes(Patten, strlen(Patten));
-//printf("%s\n",NewPatten);
+  printf("\e[33mEnter target String\e[0m>>");
+  String = Read_Line();
+  printf("\e[33mEnter Patten String\e[0m>>");
+  Patten = Read_Line();
+  char *NewPatten = DelErrorSlashes(Patten, strlen(Patten));
   bool flag;
-  flag=RegPatten(String,NewPatten);
+  flag=IsRegMatch(String,NewPatten);
   if (flag)
-    printf("匹配成功！\n");
+    printf("\e[36m匹配成功！\e[0m\n");
   else
-    printf("匹配失败！\n");
+    printf("\e[35m匹配失败！\e[0m\n");
+  struct MatchTerm resultP;
+  resultP=ComplMatch(String,NewPatten);
+  int l = resultP.len;
+  for (int i=0;i<l;++i){
+    printf("\e[32m%s\e[0m\n",resultP.pstring[i]);
+  }
   return 0;
 }
 
-bool RegPatten(char *s, char *p){
-  bool result = true;
-  if (*s==nt&&*p==nt);
-      /*-----p='\0',s='\0'-----*/
-  else if (*p!=nt){
+bool IsRegMatch(char *s, char *p){
+  bool result = false;
+  if (*s==NL&&*p==NL)
+    /*-----p='\0',s='\0'-----*/
+    result=true;
+  else if (*p!=NL){
       /*-----p = ###'\0',s = #####'\0' or '\0' -----*/
-    if (*p==star){
+    if (*p==STAR){
       /*-----p = *#### -----*/
-      result = RegPatten(s,p+1);
-    }else if (*p==dot&&*(p+1)==star){
+      result = IsRegMatch(s,p+1);
+    }else if (*p==DOT&&*(p+1)==STAR){
       /*-----p=.*### ------*/
-      result=false;
       for (;;){
-	if (RegPatten(s,p+2)){
+	if (IsRegMatch(s,p+2)){
 	  result=true;
 	  break;
 	}
-	if (*(s++)==nt)
+	if (*(s++)==NL)
 	  break;
       }
-    }else if (*p==dot&&*(p+1)!=star){
+    }else if (*p==DOT&&*(p+1)!=STAR){
       /*-----p=.####------*/
-      if (*s==nt)
-	result = false;
-      else
-	result=RegPatten(s+1,p+1);
-    }else if (*p==slashes&&*(p+1)==slashes&&*(p+2)==star){
+      if (*s!=NL)
+	result=IsRegMatch(s+1,p+1);
+    }else if (*p==SLASHES&&*(p+1)==SLASHES&&*(p+2)==STAR){
       /*----p = \\* ------*/
-      result=false;
       do {
-	if (RegPatten(s,p+3)){
+	if (IsRegMatch(s,p+3)){
 	  result=true;
 	  break;
 	}
-      }while(*(s++)==slashes);
-    }else if (*p==slashes&&*(p+1)==slashes&&*(p+2)!=star){
+      }while(*(s++)==SLASHES);
+    }else if (*p==SLASHES&&*(p+1)==SLASHES&&*(p+2)!=STAR){
       /*-----p = \\A -----*/
-      if (*s==slashes){
-	result=RegPatten(s+1,p+2);
-      }else {
-	result=false;
+      if (*s==SLASHES){
+	result=IsRegMatch(s+1,p+2);
       }
-    }else if (*p==slashes&&*(p+1)==star&&*(p+2)==star){
+    }else if (*p==SLASHES&&*(p+1)==STAR&&*(p+2)==STAR){
       /*-----p = \** -----*/
-      result=false;
       do {
-	if (RegPatten(s,p+3)){
+	if (IsRegMatch(s,p+3)){
 	  result=true;
 	  break;
 	}
-      }while(*(s++)==star);
-    }else if (*p==slashes&&*(p+1)==star&&*(p+2)!=star){
+      }while(*(s++)==STAR);
+    }else if (*p==SLASHES&&*(p+1)==STAR&&*(p+2)!=STAR){
       /*-----p = \*A -----*/
-      if (*s==star){
-	result=RegPatten(s+1,p+2);
-      }else {
-	result=false;
+      if (*s==STAR){
+	result=IsRegMatch(s+1,p+2);
       }
-    }else if (*p==slashes&&*(p+1)==dot&&*(p+2)==star){
+    }else if (*p==SLASHES&&*(p+1)==DOT&&*(p+2)==STAR){
       /*-----p = \.* -----*/
-      result=false;
       do {
-	if (RegPatten(s,p+3)){
+	if (IsRegMatch(s,p+3)){
 	  result=true;
 	  break;
 	}
-      }while(*(s++)==dot);
-    }else if (*p==slashes&&*(p+1)==dot&&*(p+2)!=star){
+      }while(*(s++)==DOT);
+    }else if (*p==SLASHES&&*(p+1)==DOT&&*(p+2)!=STAR){
       /*-----p = \.A -----*/
-      if (*s==dot){
-	result=RegPatten(s+1,p+2);
-      }else {
-	result=false;
+      if (*s==DOT){
+	result=IsRegMatch(s+1,p+2);
       }
-    }else if (*(p+1)==star){
+    }else if (*(p+1)==STAR){
       /*------p = A*-----*/
-      result=false;
       do {
-	if (RegPatten(s,p+2)){
+	if (IsRegMatch(s,p+2)){
 	  result=true;
 	  break;
 	}
       }while(*(s++)==*p);
-    }else if (*(p+1)!=star){
+    }else if (*(p+1)!=STAR){
       /*------p = A##-----*/
       if (*s==*p){
-	result=RegPatten(s+1,p+1);
-      }else {
-	result=false;
+	result=IsRegMatch(s+1,p+1);
       }
     }
   }
-  else {
-    /*----- p='\0',s=##'\0' -----*/
-    result=false;
-  }
   return result;
 }
-char* read_line(void){
+char* Read_Line(void){
   int contain = BLOCK;
   int count = 0;
   int ch;
@@ -152,30 +143,67 @@ char* read_line(void){
   return p;
 }
 
-char* CheckSlashes(char *p, int len){
+char* DelErrorSlashes(char *p, int len){
   char *t=(char*)malloc(sizeof(char)*(len+1));
   char *tmp=t;
   for (;;){
-    if (*p==slashes&&*(p+1)==slashes){
+    if (*p==SLASHES&&*(p+1)==SLASHES){
       /*----\\----*/
       *(tmp++)=*(p++);
       *(tmp++)=*(p++);
-    }else if (*p==slashes&&*(p+1)==star){
+    }else if (*p==SLASHES&&*(p+1)==STAR){
       /*----\*----*/
       *(tmp++)=*(p++);
       *(tmp++)=*(p++);
-    }else if (*p==slashes&&*(p+1)==dot){
+    }else if (*p==SLASHES&&*(p+1)==DOT){
       /*----\.----*/
       *(tmp++)=*(p++);
       *(tmp++)=*(p++);
-    }else if (*p==slashes) {
+    }else if (*p==SLASHES) {
       p++;
     }else {
       *(tmp++)=*(p++);
     }
-    if (*p==nt)
+    if (*p==NL)
       break;
   }
-  *tmp=nt;
+  *tmp=NL;
   return t;
+}
+
+struct MatchTerm ComplMatch(char *s, char *p){
+  struct MatchTerm mt;
+  char tmpch;
+  char *t=s;
+  bool flag;
+  int count=0;
+  int start=0;
+  do {
+    int end=0;
+    bool ismt = false;
+    for (int i=0;*t!=NL;i++){
+      tmpch=*(++t);
+      *t=NL;
+      flag = IsRegMatch(s+start,p);
+      *t=tmpch;
+      if (flag){
+	end=i;
+	ismt=true;
+      }
+    }
+    if (ismt){
+      char *ctp=(char*)malloc(sizeof(char)*(end+2));
+      for (int k=0;k<end+1;++k)
+	ctp[k]=s[start+k];
+      ctp[end+1]=NL;
+      mt.pstring[count]=ctp;
+      count++;
+    }
+    start += end+1;
+    t=&s[start];
+    if (*t==NL)
+      break;
+  }while(count<NUM_MAX_MATCH);
+  mt.len=count;
+  return mt;
 }
